6890aec6360387a3ba094f4b30c2680e319389fc
Michael Natanael <mnatanael87@gmail.com>
2024-05-10 21:12:09 +0700

    comment all print at python_tts

--- python_tts.py
+++ python_tts.py
@@ -9,12 +9,12 @@ def python_tts(text, output_path):
 
     """ RATE"""
     rate = engine.getProperty('rate')   # getting details of current speaking rate
-    print (rate)                        #printing current voice rate
+    # print (rate)                        #printing current voice rate
     engine.setProperty('rate', 210)     # setting up new voice rate
 
     """VOLUME"""
     volume = engine.getProperty('volume')   #getting to know current volume level (min=0 and max=1)
-    print (volume)                          #printing current volume level
+    # print (volume)                          #printing current volume level
     engine.setProperty('volume',1.0)    # setting up volume level  between 0 and 1
 
     """VOICE"""
